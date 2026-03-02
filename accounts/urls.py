483955ae122c6c1d23da0d7232f47a63b0f00b6f
Daniel Hernàndez <iurse2@gmail.com>
2026-03-02 15:13:59 -0600

    Cambio de paneles

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('admin-panel/', views.admin_panel, name='admin_panel'),
+    path('user-panel/', views.user_panel, name='user_panel'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request,
+                            username=username,
+                            password=password)
+
+        if user is not None:
+            login(request, user)
+
+            if user.is_superuser:
+                return redirect("admin_panel")
+            else:
+                return redirect("user_panel")
+
+    return render(request, "accounts/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required
+def admin_panel(request):
+    if not request.user.is_superuser:
+        return redirect("user_panel")
+
+    return render(request, "accounts/admin_panel.html")
+
+
+@login_required
+def user_panel(request):
+    return render(request, "accounts/user_panel.html")
